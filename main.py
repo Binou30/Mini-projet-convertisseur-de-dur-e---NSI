054c8673dac20474899c6850e3747c3622db43ab
Alban <binou3.0@MBA-2026.local>
2026-09-19 20:19:13 +0200

    Start first function

--- main.py
+++ main.py
@@ -2,16 +2,12 @@
 ##
 import time
 ##
-## Partie informations
-##
 print("Bonjour, je convertis des secondes en jours, heures, minutes et secondes et des jours, heures, minutes et secondes en secondes !")
 time.sleep(1)
 print("Que veux tu faire?")
 time.sleep(1)
 print("1 : Convertir des secondes en jours, heures, minutes et secondes\n2 : Convertir des jours, heures, minutes et secondes en secondes\n3 : Quitter")
 time.sleep(3)
-##
-## Partie conversion et choix
 ##
 choix=input("Alors, qu'est ce que tu choisis ?")
 while choix!="1" and choix!="2" and choix!="3":
@@ -20,7 +16,14 @@ while choix!="1" and choix!="2" and choix!="3":
     choix=input("Alors, qu'est ce que tu choisis ?")
 choix=int(choix)
 if choix==1:
-    ## Partie de Alban T (il faudra enlever le print en dessous)
+    nbint="false"
+    while nbint=="false":
+        try:
+            sec1=int(input("Entre un nombre de secondes à convertir"))
+            nbint="true"
+        except ValueError:
+            print("Entrée incorrecte")
+            time.sleep(1)
     print("Cette partie n'est pas encore faite")
 else:
     print("Cette partie n'est pas encore faite")

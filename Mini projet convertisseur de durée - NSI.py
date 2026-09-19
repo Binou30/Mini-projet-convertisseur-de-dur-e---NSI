6a775db585d566610aae2f29f462d492382650a8
Alban <binou3.0@MBA-2026.local>
2026-09-19 09:35:22 +0200

    First commit - menu part

---
+++ Mini projet convertisseur de durée - NSI.py
@@ -0,0 +1,23 @@
+## Mini projet - Convertisseur de durée - AlbanD/T, Victor
+##
+import time
+##
+print("Bonjour, je convertis des secondes en jours, heures, minutes et secondes et des jours, heures, minutes et secondes en secondes !")
+time.sleep(1)
+print("Que veux tu faire?")
+time.sleep(1)
+print("1 : Convertir des secondes en jours, heures, minutes et secondes\n2 : Convertir des jours, heures, minutes et secondes en secondes\n3 : Quitter")
+time.sleep(3)
+choix=input("Alors, qu'est ce que tu choisis ?")
+while choix!="1" and choix!="2" and choix!="3":
+    print("Entrée incorrecte")
+    time.sleep(1)
+    choix=input("Alors, qu'est ce que tu choisis ?")
+choix=int(choix)
+if choix==1:
+    ## Partie de Alban T (il faudra enlever le print en dessous)
+    print("Cette partie n'est pas encore faite")
+else:
+    print("Cette partie n'est pas encore faite")
+    ## Partie de Victor (il faudra enlever le print en dessous)
+print("Au revoir !")
